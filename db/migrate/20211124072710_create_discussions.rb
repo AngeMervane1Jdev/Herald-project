960342acf2fb6c9f824e4fb5cf9cda9602636f75
angenoutchogbe@gmail.com <onpasse1@gmail.com>
2021-12-07 14:26:23 +0000

    init

--- db/migrate/20211124072710_create_discussions.rb
+++ db/migrate/20211124072710_create_discussions.rb
@@ -1,7 +1,7 @@
 class CreateDiscussions < ActiveRecord::Migration[6.1]
   def change
     create_table :discussions do |t|
-      t.string :type
+      t.string :category
       t.string :titre
       t.timestamps
     end
--- db/migrate/20211125222248_chage_column.rb
+++
@@ -1,6 +0,0 @@
-class ChageColumn < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :discussions, :type, :category
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
-  end
-end
